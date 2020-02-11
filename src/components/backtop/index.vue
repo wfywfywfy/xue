bdd23d0442dffe59a6478576d3e0ea85112cae5f
wfywfywfy <798527915@qq.com>
2020-02-11 13:09:30 +0800

    pp

---
+++ src/components/backtop/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="backtop">
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'backtop',
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+  computed: {}
+}
+</script>
+<style scoped lang="less">
+</style>
